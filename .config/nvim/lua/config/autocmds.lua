0e7fbed5079b975de49b7064a74ca27ce3255ccb
Unathi <ukskosana@gmail.com>
2023-07-24 22:31:37 +0200

    feat(neovim): add last position and automatically reload autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -30,27 +30,55 @@ autocmd("BufWritePre", {
   command = ":silent lua vim.lsp.buf.format()"
 })
 
+
 -- Don"t auto commenting new lines
 autocmd("BufEnter", {
   pattern = "",
   command = "set fo-=c fo-=r fo-=o"
 })
 
+
+--- Set shiftwidth and tabstops to 2 for miscellaneous files
 autocmd("Filetype", {
-  pattern = { "xml", "html", "xhtml", "css", "scss", "javascript", "typescript", "yaml", "lua" },
+  pattern = { "xml", "html", "xhtml", "css", "scss", "yaml", "lua", "javascript", "typescript" },
   command = "setlocal shiftwidth=2 tabstop=2"
 })
 
--- Set colorcolumn
--- autocmd("Filetype", {
---     pattern = {"python", "rst", "c", "cpp"},
---     command = "set colorcolumn=80"
--- })
 
+--- Enable wrapping in git commits, markdown and text
 autocmd("Filetype", {
   pattern = { "gitcommit", "markdown", "text" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
+  end
+})
+
+-- Automatically reload currently open buffer in case it was changed outside,
+-- very useful for log files (.csv, .dat, .log, .out) that being updated
+-- in real time.
+-- <https://stackoverflow.com/questions/62100785/auto-reload-file-and-in-neovim-and-auto-reload-nerbtree>
+autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" }
+})
+
+
+--- Reopen from last position
+autocmd("BufReadPost *", {
+  callback = function()
+    local ft = vim.opt_local.filetype:get()
+    -- don't apply to git messages
+    if (ft:match('commit') or ft:match('rebase')) then
+      return
+    end
+    -- get position of last saved edit
+    local markpos = vim.api.nvim_buf_get_mark(0, '"')
+    local line = markpos[1]
+    local col = markpos[2]
+    -- if in range, go there
+    if (line > 1) and (line <= vim.api.nvim_buf_line_count(0)) then
+      vim.api.nvim_win_set_cursor(0, { line, col })
+    end
   end
 })
